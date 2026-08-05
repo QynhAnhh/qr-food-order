6ff017a277a84345d2b49f0e19ffde1f02a0bfc1
Do Nguyen Quynh Anh <irisdo.qa@gmail.com>
2026-08-06 01:10:44 +0700

    feat(middleware): Thêm auth và error middlewares

---
+++ backend/src/middlewares/auth.mid.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken');
+const env = require('../config/env.config');
+const ERROR_CODES = require('../constants/errorCodes');
+
+    const verifyToken = (req, res, next) => {
+        const authHeader = req.header.authorazition;
+        if (!authHeader || !authHeader.startWith('Bearer')) {
+            return res.status(ERROR_CODES.UNAUTHORIZED).json({message: 'Bạn chưa đăng nhập'});
+        }
+        const token = authHeader.split(' ')[1];
+        try {
+            const decoded = jwt.verify(token, env.JWT_SECRET);
+            req.user = decoded;
+            next();}
+        catch (error){
+            return res.status(ERROR_CODES.UNAUTHORIZED).json({message: "Thẻ đăng nhập hết hạn hoặc là giả!"});
+        };
+
+    const checkRole = (...allowedRoles) => {
+            return (req, res, next) => {
+                if(!req.user || !allowedRoles.includes(req.user.role)) {
+                    return res.status(ERROR_CODES.FORBIDDEN).json({message: "Bạn không có quyền"})  ;
+                }
+                next();
+            };
+        };
+    
+        module.exports = {verifyToken, checkRole};
+
---
+++ backend/src/middlewares/error.mid.js
@@ -0,0 +1,21 @@
+const logger = require('../lib/logger');
+const ERROR_CODES = require('../constants/errorCodes');
+
+const errorHandler = (err, req, res, next) => {
+    logger.error(`${req.method} ${req.url} - ${err.message}`);
+
+    const statusCode = err.statusCode || ERROR_CODES.SERVER_ERROR;
+
+    res.status(statusCode).json({
+        success: false,
+        message: err.message || "Lỗi hệ thống!",
+        stack: process.env.NODE_ENV === "development" ? err.stack : undefined
+    });
+};
+
+module.exports = errorHandler;
+
+/* trong err bao gồm .message . statusCode . stack
+ trong req bao gồm .method .url 
+ trong res bao gồm .status(mã) .json({})
+*/
