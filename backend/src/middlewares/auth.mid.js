32eaf1a15a01e35cc6db2fe07411a1469131faa7
Do Nguyen Quynh Anh <irisdo.qa@gmail.com>
2026-08-06 01:14:10 +0700

    fix(middleware): Sửa lỗi cú pháp trong auth.mid.js

--- backend/src/middlewares/auth.mid.js
+++ backend/src/middlewares/auth.mid.js
@@ -3,23 +3,24 @@ const env = require('../config/env.config');
 const ERROR_CODES = require('../constants/errorCodes');
 
     const verifyToken = (req, res, next) => {
-        const authHeader = req.header.authorazition;
-        if (!authHeader || !authHeader.startWith('Bearer')) {
+        const authHeader = req.headers.authorization;
+        if (!authHeader || !authHeader.startsWith('Bearer ')) {
             return res.status(ERROR_CODES.UNAUTHORIZED).json({message: 'Bạn chưa đăng nhập'});
         }
         const token = authHeader.split(' ')[1];
         try {
             const decoded = jwt.verify(token, env.JWT_SECRET);
             req.user = decoded;
-            next();}
-        catch (error){
+            next();
+        } catch (error){
             return res.status(ERROR_CODES.UNAUTHORIZED).json({message: "Thẻ đăng nhập hết hạn hoặc là giả!"});
-        };
+        }
+    };
 
     const checkRole = (...allowedRoles) => {
             return (req, res, next) => {
                 if(!req.user || !allowedRoles.includes(req.user.role)) {
-                    return res.status(ERROR_CODES.FORBIDDEN).json({message: "Bạn không có quyền"})  ;
+                    return res.status(ERROR_CODES.FORBIDDEN).json({message: "Bạn không có quyền"});
                 }
                 next();
             };
