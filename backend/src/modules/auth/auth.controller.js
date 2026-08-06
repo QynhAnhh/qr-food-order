8b69db088ddfac2a48e578367462b26c6ceb31c4
Do Nguyen Quynh Anh <irisdo.qa@gmail.com>
2026-08-07 01:30:03 +0700

    feat(auth): Thêm tính năng đăng nhập và cấp phát JWT

---
+++ backend/src/modules/auth/auth.controller.js
@@ -0,0 +1,49 @@
+const PrismaClient = require("@prisma/client").PrismaClient;
+const prisma = new PrismaClient();
+const { comparePassword } = require("../../lib/hash");
+const jwt = require("jsonwebtoken");
+const ERROR_CODES = require("../../constants/errorCodes");
+const env = require("../../configs/env.config");
+
+const login = async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const user = await prisma.user.findUnique({
+      where: { username },
+    });
+    if (!user) {
+      const error = new Error("Tài khoản không tồn tại!");
+      error.statusCode = ERROR_CODES.NOT_FOUND;
+      throw error;
+    }
+    const isPasswordValid = await comparePassword(password, user.password);
+    if (!isPasswordValid) {
+      const error = new Error("Mật khẩu không chính xác!");
+      error.statusCode = ERROR_CODES.UNAUTHORIZED;
+      throw error;
+    }
+
+    const payload = {
+      id: user.id,
+      role: user.role,
+    };
+
+    const token = jwt.sign(payload, env.JWT_SECRET, { expiresIn: "7d" });
+
+    res.status(200).json({
+      success: true,
+      message: "Đăng nhập thành công!",
+      data: {
+        token: token,
+        user: {
+          id: user.id,
+          username: user.username,
+          role: user.role,
+        },
+      },
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+module.exports = { login };
