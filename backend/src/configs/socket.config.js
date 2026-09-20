77de80d68eb77e0c46c9926fc0e1f0dfacd9bb6d
Do Nguyen Quynh Anh <irisdo.qa@gmail.com>
2026-09-21 02:29:48 +0700

    feat(socket): add initial real-time shared cart events and redis logic

--- backend/src/configs/socket.config.js
+++ backend/src/configs/socket.config.js
@@ -1,35 +1,65 @@
-const {Server} = require('socket.io');
-const logger = require('../lib/logger');
+const { Server } = require("socket.io");
+const logger = require("../lib/logger");
+const { redisClient } = require("./redis.config");
+const SOCKET_EVENTS = require("../constants/socketEvents");
 
 let ioInstance;
 
-function initSocket(httpSv){
-    ioInstance = new Server(httpSv, {
-        cors:{
-            origin: '*',
-        }
+function initSocket(httpSv) {
+  ioInstance = new Server(httpSv, {
+    cors: {
+      origin: "*",
+    },
+  });
+
+  ioInstance.on("connection", (socket) => {
+    logger.info(`Một trình duyệt vừa kết nối với id: ${socket.id}`);
+
+    socket.on("join_table", (tableId) => {
+      socket.join(`table_${tableId}`);
+      logger.info(`Trình duyệt ${socket.id} đã vào bàn số ${tableId}`);
     });
 
-    ioInstance.on('connection', (socket) => {
-        logger.info(`Một trình duyệt vừa kết nối với id: ${socket.id}`);
+    socket.on(SOCKET_EVENTS.ADD_ITEM, async (data) => {
+      const { tableId, item } = data;
+      const cartKey = `cart:${tableId}`;
+      try {
+        let cartData = await redisClient.get(cartKey);
+        let cart = cartData ? JSON.parse(cartData) : [];
 
-        socket.on('join_table', (tableId) => {
-            socket.join(`table_${tableId}`);
-            logger.info(`Trình duyệt ${socket.id} đã vào bàn số ${tableId}`);
-       });
+        const existingItemIndex = cart.findIndex(   // findIndex: tìm kiếm vị trí lặp qua một mảng và trả về vị trí của phần tử đầu tiên thỏa mãn điều kiện, nếu kh tìm thấy thì trả về -1
+          (cartItem) => cartItem.menuItemId === item.menuItemId,  // điều kiện
+        );
+        if (existingItemIndex > -1) {
+          cart[existingItemIndex].quantity += item.quantity;
+        } else {
+          cart.push(item);
+        }
+        await redisClient.set(cartKey, JSON.stringify(cart));
 
-        socket.on('disconnect', () => {
-        logger.info(`Trình duyệt ${socket.id} đã ngắt kết nối`);
-       });
-    })
-    return ioInstance;
+        ioInstance
+          .to(`table_${tableId}`)
+          .emit(SOCKET_EVENTS.CART_UPDATED, cart);
+        logger.info(
+          `Bàn ${tableId} vừa thêm món ${item.menuItemId} vào giỏ hàng.`,
+        );
+      } catch (error) {
+        logger.error("Lỗi khi cập nhật giỏ hàng", error);
+      }
+    });
+
+    socket.on("disconnect", () => {
+      logger.info(`Trình duyệt ${socket.id} đã ngắt kết nối`);
+    });
+  });
+  return ioInstance;
 }
 
 function getIo() {
-    if (!ioInstance) {
-        throw new Error("Socket chưa được khởi tạo!");
-    }
-    return ioInstance;
+  if (!ioInstance) {
+    throw new Error("Socket chưa được khởi tạo!");
+  }
+  return ioInstance;
 }
 
 module.exports = { initSocket, getIo };
--- backend/src/constants/socketEvents.js
+++ backend/src/constants/socketEvents.js
@@ -3,10 +3,12 @@ const SOCKET_EVENTS = {
   ADD_ITEM: 'add_item',
   REMOVE_ITEM: 'remove_item',
   REQUEST_CART_STATE: 'request_cart_state', // Khách bị rớt mạng, vô lại xin dữ liệu giỏ hàng
-
+  CART_UPDATED: 'cart_updated',
+  
   // Liên quan đến Đơn hàng (Đã chốt)
   ORDER_NEW: 'order:new',
-  ORDER_STATUS_CHANGE: 'order:statusChange' // Báo cho Bếp/Khách là trạng thái món vừa đổi
+  ORDER_STATUS_CHANGE: 'order:statusChange', // Báo cho Bếp/Khách là trạng thái món vừa đổi
+  PAYMENT_SUCCESS: 'payment:success'
 };
 
 module.exports = SOCKET_EVENTS;
